18a34641f3d731819c53b1d47f9ad64b4f24801b
Edson Marques <edson@ms3ti.com.br>
2018-05-24 10:05:56 -0300

    Inicial GIT
    Inicial

---
+++ _saida.php
@@ -0,0 +1,64 @@
+﻿<?php
+ini_set('display_errors',0);
+error_reporting(E_ALL);
+set_time_limit(0);
+
+require 'Excel/reader.php';
+
+$data = new Spreadsheet_Excel_Reader();
+$data->setOutputEncoding('UTF-8');
+
+
+//echo "<pre>";
+$nomearquivo = explode('.',implode(explode(' ',$_FILES['upfile']['name'])));
+$nomearquivo = $nomearquivo[0];
+
+//print_r($nomearquivo); die();
+
+
+if(!empty($_FILES['upfile']) && ($_FILES['upfile']['type'] == "application/vnd.ms-excel" || $_FILES['upfile']['type'] == "application/x-msexcel")) {
+	$data->read($_FILES['upfile']['tmp_name']);
+}
+else {
+	//$data->read('test.xls');
+	//echo "<pre>";
+	//print_r($_FILES);
+	echo "Arquivo Inválido!<br><br>Aceito somente formato: <b>Pasta de Trabalho do Excel 97-2003 (*.xls)</b>"; die();
+}
+
+header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header ("Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT");
+header ("Cache-Control: no-cache, must-revalidate");
+header ("Pragma: no-cache");
+header ("Content-type: application/csv");
+header ("Content-Type: text/html; charset=utf-8");
+header ("Content-Disposition: attachment; filename=".$arquivo."" );
+header ("Content-Disposition: attachment; filename=".$nomearquivo.".csv");
+
+$arq_saida = '';
+	for ($i = 1; $i <= $data->sheets[0]['numRows']; $i++) {
+			for ($j = 1; $j <= $data->sheets[0]['numCols']; $j++) {
+				$celldata = utf8_encode((!empty($data->sheets[0]['cells'][$i][$j])) ? $data->sheets[0]['cells'][$i][$j] : "&nbsp;");
+				//$arq_saida .= "'".trim($celldata)."',";
+				if($j != 1)
+				{
+					echo ",";
+				}
+				echo "\"".trim(str_replace("&nbsp;", " ", str_replace(chr(13), " ", str_replace(chr(10), " ", str_replace("\"", "'", $celldata)))))."\"";
+			}
+			echo "
+";
+	}
+
+//echo $arq_saida;
+//header('Content-Disposition: attachment; filename=example.csv');
+
+
+/*
+header('Content-Type: application/csv');
+header('Content-Disposition: attachment; filename='.$nomearquivo.'.csv');
+header('Content-Type: text/html; charset=utf-8');
+header('Pragma: no-cache');
+*/
+
+?>
---
+++ index.php
@@ -0,0 +1,69 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt-br">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>MS3TI Conversor de Planilhas</title>
+
+<style>
+h1 {
+	font-family:Verdana;
+}
+fieldset {
+	border-style:ridge;
+	border-width:1;
+	border-collapse:collapse;
+	font-family:Verdana;
+	font-size:12px;
+}
+</style>
+
+</head>
+<body>
+
+
+<table width="850" align="center" valign="middle">
+	<tr>
+		<td colspan="2" align="center">
+			<img src="ms3ti_logo.png">
+			<h1 style="font:Verdana">Conversor de Planilhas</h1>
+		</td>
+	</tr>
+	<tr>
+		<td align="center">
+			<img src="ferramentas.jpg" border="0">
+		</td>
+		<td align="center">
+			<form action="_saida.php" method="post" enctype="multipart/form-data">
+				<fieldset>
+					<legend>Converter Arquivos do Excel</legend>
+					<label>
+						Arquivo do Excel (.xls): 
+						<input type="file" name="upfile" />
+					</label>
+					<br>
+					<label>
+						Formato de Saída: 
+						<select disabled>
+							<option value="CSV">CSV</option>
+						</select>
+					</label>
+					<label>
+						Codificação de Caracteres: 
+						<select disabled>
+							<option value="UTF-8">UTF-8</option>
+						</select>
+					</label>
+					<br>
+					<input type="submit" value="Converter" />
+				</fieldset>
+			</form>
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+			<font face="Verdana" size="1">Powered by <a href="http://www.ms3ti.com.br">MS3TI Tecnologia da Informação</a>.</font>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
